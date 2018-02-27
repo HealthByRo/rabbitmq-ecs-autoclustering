8f2b072e0e0698282d77b86e8856e26c157cf9b1
Sabin Iacob <iacobs@m0n5t3r.info>
2018-02-28 00:40:44 +0200

    add support for instance tags as well, take 2: rabbitmq does not like keys containing ":", use Name instead

--- docker-image/docker-cluster-entrypoint.sh
+++ docker-image/docker-cluster-entrypoint.sh
@@ -6,9 +6,9 @@
 [ -n "$AWS_USE_PRIVATE_IP" ] && echo "cluster_formation.aws.use_private_ip = $AWS_USE_PRIVATE_IP" >> /etc/rabbitmq/rabbitmq.conf
 [ -n "$LOG_LEVEL" ] && echo "log.console.level = $LOG_LEVEL" >> /etc/rabbitmq/rabbitmq.conf
 
-if [ -n "$AWS_CLOUDFORMATION_STACK_NAME" ]; then
+if [ -n "$AWS_INSTANCE_NAME" ]; then
     sed -i -e "/cluster_formation.aws.use_autoscaling_group = true/d" /etc/rabbitmq/rabbitmq.conf
-    echo "cluster_formation.aws.instance_tags.aws:cloudformation:stack-name = $AWS_CLOUDFORMATION_STACK_NAME" >> /etc/rabbitmq/rabbitmq.conf
+    echo "cluster_formation.aws.instance_tags.Name = $AWS_INSTANCE_NAME" >> /etc/rabbitmq/rabbitmq.conf
 fi
 
 source /usr/local/bin/docker-entrypoint.sh
